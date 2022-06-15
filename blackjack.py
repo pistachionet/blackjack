641c32b4eb9c372f27b1506d3871b60f52348751
Navid Misaghian <navid@debian-n>
2022-06-14 23:32:39 -0400

    First Commit

---
+++ blackjack.py
@@ -0,0 +1,5 @@
+#Project Objectives:
+#Build a terminal program using Python
+#Add at least one interactive feature using input()
+#Use Git version control
+#Use the command line and file navigation
--- main/blackjack.py
+++
@@ -1,4 +0,0 @@
-#Build a terminal program using python
-#Add at least one interactive feature using input()
-#Use Git version control
-#Use the command line and file navigation
