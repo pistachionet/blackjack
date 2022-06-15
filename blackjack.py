ebb3530b822077adcea23b8519127ac87984887d
Navid Misaghian <78292140+pistachionet@users.noreply.github.com>
2022-06-14 23:43:30 -0400

    Add files via upload

---
+++ blackjack.py
@@ -0,0 +1,14 @@
+#Project Objectives:
+#Build a terminal program using Python
+#Add at least one interactive feature using input()
+#Use Git version control
+#Use the command line and file navigation
+
+
+
+class Blackjack:
+    pass
+
+
+    def __init__(self):
+        pass
